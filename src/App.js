57b47064ea9969665873ebefb844043a8be60d0f
kathir2602 <kathir@crayond.co>
2022-03-06 14:25:56 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">RAY-D-CODE</h1>
+      <h4 className="sub-heading">Solve With Tech That Matters !</h4>
+    </div>
+  );
+};
+
+export default App;
